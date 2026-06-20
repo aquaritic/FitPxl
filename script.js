6ded89c0414ed4f4bd2c2791ab392a713d2f809b
aquaritic <krish.s.lad@gmail.com>
2026-06-20 14:57:23 -0500

    Added a font using google fonts, added a player stats area, revamped a lot of CSS, and made the js to update some things, a lot of stuff is still messy though.

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-let xp = 0;
 let level = 1;
+let xp = 0;
+let workouts = 0;
 
 const fill = document.querySelector(".xpAmount");
+
+document.getElementById("logWorkout").addEventListenerer("click", () => {
+    workouts++;
+    xp+=25;
+
+    if( xp >= level * 100){
+        xp = 0;
+        level++
+    }
+
+    document.getElementById("level").textContent = level;
+    document.getElementById("xp").textContent = xp;
+    document.getElementById("workouts").textContent = workouts;
+    document.getElementById("xpFill").style.width = (xp / (level*100)) * 100 + "%";
+
+});
 
 function updateXP(){
     const toNext = level * 100;
